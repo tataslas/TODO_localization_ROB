c10a41bc3e40473ccf36cb9598e877f7e6013a84
tataslas <38156502+tataslas@users.noreply.github.com>
2019-05-17 00:27:41 +0200

    1, 2, 3, las landmarks no dan igual que las del profe

--- dos.m
+++ dos.m
@@ -1,18 +1,18 @@
 
 %Apartado 2
+
+%Pasamos las distancias de polar a cartesianas, para obtener las X e Y de
+%las landmarks en el reference frame del robot
 for i=1:523
     for j=2:361
-        land_x(i,j) = (lds_dis(i,j)*cosd(j))/1000;
-        land_y(i,j) = (lds_dis(i,j)*sind(j))/1000;
+        ldx_RFr(i,j) = (lds_dis(i,j)*cosd(j))/1000;
+        ldy_RFr(i,j) = (lds_dis(i,j)*sind(j))/1000;
     end
 end
 
-%Calculo ldx y ldy
+%Plot landmarks en el reference frame del robot
 for k=1:522
-    Tw_r = transl(matrix_pose(k,1), matrix_pose(k,2),0)*trotz(matrix_pose(k,3));
-    for l=1:360
-        aux = Tw_r*[land_x(k,l); land_y(k,l); 0; 0];
-        ldx_nuestra(k,l) = aux(1);
-        ldy_nuestra(k,l) = aux(2);
-    end
+    scatter(ldx_RFr(k,:), ldy_RFr(k,:));
+    axis([-3 3 -2 4]); 
+    hold on;
 end
---
+++ tres.m
@@ -0,0 +1,32 @@
+
+%Apartado 3
+
+%Calculo ldx y ldy pasamos del RF del robot al RF del mundo
+for i=1:522
+    Tw_r = transl(matrix_pose(i,1), matrix_pose(i,2),0)*trotz(matrix_pose(i,3));
+    for j=1:360
+        aux = Tw_r*[land_x(i,j); land_y(i,j); 0; 0];
+        ldx_RFw(i,j) = aux(1);
+        ldy_RFw(i,j) = aux(2);
+    end
+end
+
+%Plot landmarks RFw
+Robot= [0 -0.2 0 1;0.4 0 0 1;0 0.2 0 1]';% The Robot icon is a triangle 
+for k=1:522
+    patch(Robot(1,:), Robot(2,:),'b'); %Pintamos el robot
+    for i=1:4 % plotting the 4 Land Marks 
+        circle (LandMark(i,:)',0.15, 'LineWidth',2);
+    end
+    
+    scatter(ldx_RFw(k,:), ldy_RFw(k,:));
+    axis([-3 3 -2 4]); 
+    hold on;
+end
+
+%Lo que deberia de dar
+for k=1:522
+    scatter(ldx(k,:), ldy(k,:));
+    axis([-3 3 -2 4]); 
+    hold on;
+end
