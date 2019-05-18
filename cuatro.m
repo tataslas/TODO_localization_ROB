4270109af43380b7c2a13436d238b40e6478c9af
tataslas <38156502+tataslas@users.noreply.github.com>
2019-05-18 16:30:04 +0200

    cuatro!!

--- cuatro.m
+++ cuatro.m
@@ -28,29 +28,35 @@ for i=1:522
         %%%%%%
         % X
         %%%%%%
-        %Arriba derecha
+        %Arriba derecha (ur)
         if ldx_RFw(i,j) > (0.5) && ldx_RFw(i,j) < (2.5)
             ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
             xcount_u_r = xcount_u_r + 1;
             xsum_u_r = xsum_u_r + ldx_RFw(i,j);
             
-        %Abajo derecha
-        elseif ldx_RFw(i,j) > (0.5) && ldx_RFw(i,j) < (-2.5)
-            ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
             xcount_d_r = xcount_d_r + 1;
             xsum_d_r = xsum_d_r + ldx_RFw(i,j);
-        
-        %Abajo izquierda
-        elseif ldx_RFw(i,j) > (-0.5) && ldx_RFw(i,j) < (-2.5)
+            
+        %Abajo derecha (dr)
+%         elseif ldx_RFw(i,j) > (0.5) && ldx_RFw(i,j) < (2.5)
+%             ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
+%             xcount_d_r = xcount_d_r + 1;
+%             xsum_d_r = xsum_d_r + ldx_RFw(i,j);
+%         
+        %Abajo izquierda (dl)
+        elseif ldx_RFw(i,j) < (-0.5) && ldx_RFw(i,j) > (-2.5)
             ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
             xcount_d_l = xcount_d_l + 1;
             xsum_d_l = xsum_d_l + ldx_RFw(i,j);
-         
-        %Arriba izquierda
-        elseif ldx_RFw(i,j) > (-0.5) && ldx_RFw(i,j) < (2.5)
-            ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
+            
             xcount_u_l = xcount_u_l + 1;
             xsum_u_l = xsum_u_l + ldx_RFw(i,j);
+         
+        %Arriba izquierda (ul)
+%         elseif ldx_RFw(i,j) < (-0.5) && ldx_RFw(i,j) > (-2.5)
+%             ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
+%             xcount_u_l = xcount_u_l + 1;
+%             xsum_u_l = xsum_u_l + ldx_RFw(i,j);
             
         else
             ldx_RFw_filtro(i,j) = 0;
@@ -63,25 +69,31 @@ for i=1:522
         if ldy_RFw(i,j) > (0.5) && ldy_RFw(i,j) < (2.5)
             ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
             ycount_u_r = ycount_u_r + 1;
-            ysum_u_r = ysum_u_r + ldx_RFw(i,j);
+            ysum_u_r = ysum_u_r + ldy_RFw(i,j);
+            
+            ycount_u_l = ycount_u_l + 1;
+            ysum_u_l = ysum_u_l + ldy_RFw(i,j);
             
         %Abajo derecha
-        elseif ldy_RFw(i,j) > (0.5) && ldy_RFw(i,j) < (-2.5)
+        elseif ldy_RFw(i,j) < (-0.5) && ldy_RFw(i,j) > (-2.5)
             ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
             ycount_d_r = ycount_d_r + 1;
-            ysum_d_r = ysum_d_r + ldx_RFw(i,j);
+            ysum_d_r = ysum_d_r + ldy_RFw(i,j);
+            
+            ycount_d_l = ycount_d_l + 1;
+            ysum_d_l = ysum_d_l + ldy_RFw(i,j);
         
         %Abajo izquierda
-        elseif ldy_RFw(i,j) > (-0.5) && ldy_RFw(i,j) < (-2.5)
-            ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
-            ycount_d_l = ycount_d_l + 1;
-            ysum_d_l = ysum_d_l + ldx_RFw(i,j);
+%         elseif ldy_RFw(i,j) < (-0.5) && ldy_RFw(i,j) > (-2.5)
+%             ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
+%             ycount_d_l = ycount_d_l + 1;
+%             ysum_d_l = ysum_d_l + ldy_RFw(i,j);
          
         %Arriba izquierda
-        elseif ldy_RFw(i,j) > (-0.5) && ldy_RFw(i,j) < (2.5)
-            ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
-            ycount_u_l = ycount_u_l + 1;
-            ysum_u_l = ysum_u_l + ldx_RFw(i,j);
+%         elseif ldy_RFw(i,j) > (0.5) && ldy_RFw(i,j) < (2.5)
+%             ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
+%             ycount_u_l = ycount_u_l + 1;
+%             ysum_u_l = ysum_u_l + ldy_RFw(i,j);
             
         else
             ldy_RFw_filtro(i,j) = 0;
@@ -102,7 +114,12 @@ landmarks_nuestras(2,3) = ysum_u_l/ycount_u_l;
 landmarks_nuestras(2,4) = ysum_d_l/ycount_d_l;
 
 %Landmarks originales
-scatter(LandMark(1,:),LandMark(2,:),200, 'r','filled');
+LandMark = [  1.5,1.5; -1.5,1.5; -1.5,-1.5;  1.5, -1.5]'; 
+figure 
+axis ([-2 2 -2 2]) 
+scatter(LandMark(1,:),LandMark(2,:),200, 'r','filled'); 
+grid on;
+hold on;
 %Plot landmarks nuestras en FR world
 scatter(landmarks_nuestras(1,:),landmarks_nuestras(2,:),200, 'b','filled');
 
