d7141dac1205226b5e9fd9a1e9dc0644da1d767a
tataslas <38156502+tataslas@users.noreply.github.com>
2019-05-19 13:23:19 +0200

    4 corregido

--- cuatro.m
+++ cuatro.m
@@ -25,79 +25,38 @@ ysum_d_l = 0;
 
 for i=1:522
     for j=1:360
-        %%%%%%
-        % X
-        %%%%%%
         %Arriba derecha (ur)
-        if ldx_RFw(i,j) > (0.5) && ldx_RFw(i,j) < (2.5)
-            ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
+        if (ldx_RFw(i,j) > (0.5) && ldx_RFw(i,j) < (2.5)) && (ldy_RFw(i,j) > (0.5) && ldy_RFw(i,j) < (2.5))
             xcount_u_r = xcount_u_r + 1;
             xsum_u_r = xsum_u_r + ldx_RFw(i,j);
             
-            xcount_d_r = xcount_d_r + 1;
-            xsum_d_r = xsum_d_r + ldx_RFw(i,j);
-            
-        %Abajo derecha (dr)
-%         elseif ldx_RFw(i,j) > (0.5) && ldx_RFw(i,j) < (2.5)
-%             ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
-%             xcount_d_r = xcount_d_r + 1;
-%             xsum_d_r = xsum_d_r + ldx_RFw(i,j);
-%         
-        %Abajo izquierda (dl)
-        elseif ldx_RFw(i,j) < (-0.5) && ldx_RFw(i,j) > (-2.5)
-            ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
-            xcount_d_l = xcount_d_l + 1;
-            xsum_d_l = xsum_d_l + ldx_RFw(i,j);
-            
-            xcount_u_l = xcount_u_l + 1;
-            xsum_u_l = xsum_u_l + ldx_RFw(i,j);
-         
-        %Arriba izquierda (ul)
-%         elseif ldx_RFw(i,j) < (-0.5) && ldx_RFw(i,j) > (-2.5)
-%             ldx_RFw_filtro(i,j) = ldx_RFw(i,j);
-%             xcount_u_l = xcount_u_l + 1;
-%             xsum_u_l = xsum_u_l + ldx_RFw(i,j);
-            
-        else
-            ldx_RFw_filtro(i,j) = 0;
-        end  
-        
-        %%%%%%
-        % Y
-        %%%%%%
-         %Arriba derecha
-        if ldy_RFw(i,j) > (0.5) && ldy_RFw(i,j) < (2.5)
-            ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
             ycount_u_r = ycount_u_r + 1;
             ysum_u_r = ysum_u_r + ldy_RFw(i,j);
             
-            ycount_u_l = ycount_u_l + 1;
-            ysum_u_l = ysum_u_l + ldy_RFw(i,j);
+        %Abajo derecha (dr)
+        elseif (ldx_RFw(i,j) > (0.5) && ldx_RFw(i,j) < (2.5)) && (ldy_RFw(i,j) < (-0.5) && ldy_RFw(i,j) > (-2.5))
+            xcount_d_r = xcount_d_r + 1;
+            xsum_d_r = xsum_d_r + ldx_RFw(i,j);
             
-        %Abajo derecha
-        elseif ldy_RFw(i,j) < (-0.5) && ldy_RFw(i,j) > (-2.5)
-            ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
             ycount_d_r = ycount_d_r + 1;
             ysum_d_r = ysum_d_r + ldy_RFw(i,j);
             
+        %Abajo izquierda (dl)
+        elseif (ldx_RFw(i,j) < (-0.5) && ldx_RFw(i,j) > (-2.5)) && (ldy_RFw(i,j) < (-0.5) && ldy_RFw(i,j) > (-2.5))
+            xcount_d_l = xcount_d_l + 1;
+            xsum_d_l = xsum_d_l + ldx_RFw(i,j);
+            
             ycount_d_l = ycount_d_l + 1;
             ysum_d_l = ysum_d_l + ldy_RFw(i,j);
-        
-        %Abajo izquierda
-%         elseif ldy_RFw(i,j) < (-0.5) && ldy_RFw(i,j) > (-2.5)
-%             ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
-%             ycount_d_l = ycount_d_l + 1;
-%             ysum_d_l = ysum_d_l + ldy_RFw(i,j);
-         
-        %Arriba izquierda
-%         elseif ldy_RFw(i,j) > (0.5) && ldy_RFw(i,j) < (2.5)
-%             ldy_RFw_filtro(i,j) = ldy_RFw(i,j);
-%             ycount_u_l = ycount_u_l + 1;
-%             ysum_u_l = ysum_u_l + ldy_RFw(i,j);
             
-        else
-            ldy_RFw_filtro(i,j) = 0;
-        end 
+        %Arriba izquierda (ul)
+        elseif (ldx_RFw(i,j) < (-0.5) && ldx_RFw(i,j) > (-2.5)) && (ldy_RFw(i,j) > (0.5) && ldy_RFw(i,j) < (2.5))
+            xcount_u_l = xcount_u_l + 1;
+            xsum_u_l = xsum_u_l + ldx_RFw(i,j);
+            
+            ycount_u_l = ycount_u_l + 1;
+            ysum_u_l = ysum_u_l + ldy_RFw(i,j);   
+        end  
     end
 end
 
